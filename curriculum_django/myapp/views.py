0a27e97833d8fec5b92fc59cac6781f13603bd7d
s33ding <robertomdiniz@protonmail.com>
2022-04-28 15:40:56 -0300

    OK

--- curriculum_django/myapp/views.py
+++ curriculum_django/myapp/views.py
@@ -24,7 +24,13 @@ def contact(request):
 def resume(request):
   mySummary_p1 = "Experienced Co-Founder with a demonstrated history of working in the information technology and services industry. Skilled in Python, SQL, Pandas, Numpy, Sympy and Command Line. Professional with a Bachelor's degree focused in Data Science and Artiﬁcial Intelligence from IESB."  
   mySummary_p2 = "Interested in AWS tools, Spark, Airﬂow, PostgreSQL, No-SQL, Docker, ETL, Scrap, CLI, Django and becoming an expert in Python."
- 
+  pos_wk1 = "Data Engineer"  
+  place_wk1 = "Ministery of Communication"  
+  finished_wk1 = "April 2022"  
+  start_wk1 = "November 2021"
+  description_wk1 = "Support the analysis of problems and technical solutions of data integration on projects related to internet connectivity in the national."  
+
+
   context = {
     "name":"Roberto Moreira Diniz",
     "role":"Data Engineer/DataScientist", 
@@ -40,10 +46,22 @@ def resume(request):
     "summary_p1": mySummary_p1,
     "summary_p2": mySummary_p2,
 
+    "pos_wk1" :  pos_wk1,
+    "place_wk1" :  place_wk1,
+    "finished_wk1" :  finished_wk1,
+    "start_wk1" :  start_wk1,
+    "description_wk1" :  description_wk1,
+
+
+
     "py1":"Pandas",
     "py2":"Subprocess",
     "py3":"Plotly",
     "py4":"Boto3",
+    
+    "SQL1":"PostgreSQL",
+    "SQL2":"SQLlite",
+    
     "linux1":"Bash",
     "linux2":"Command Line",
     "linux3":"Data Processing in Shell",
