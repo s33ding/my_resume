0d5db00a7ab74b7e17e6e485f4c10592f3677a59
s33ding <robertomdiniz@protonmail.com>
2022-04-28 21:31:49 -0300

    ok

--- curriculum_django/myapp/views.py
+++ curriculum_django/myapp/views.py
@@ -25,7 +25,7 @@ def contact(request):
 
 personalData = {
     "name":"Roberto Moreira Diniz",
-    "role":"Data Engineer/DataScientist", 
+    "role":"Data Engineer/Data Scientist", 
     "email":"robertomdiniz@protonmail.com",
     "cell_phone":"+55 (61) 98234-0088",
     "site":"https://github.com/s33ding",
@@ -35,17 +35,13 @@ personalData = {
     "linkedin":"@robertomdiniz",
     "linkedinLink":"https://www.linkedin.com/in/robertomdiniz/",
     }
-
 educationDct = {
     "university":"IESB, Brasília",
     "course":"Data Science & Artificial Intelligence, third semester.",
     }
-
 summaryDct = {
-  "p1":"Experienced Co-Founder with a demonstrated history of working in the information technology and services industry. Skilled in Python, SQL, Pandas, Numpy, Sympy and Command Line. Professional with a Bachelor's degree focused in Data Science and Artiﬁcial Intelligence from IESB.",
-  "p2":"Interested in AWS tools, Spark, Airﬂow, PostgreSQL, No-SQL, Docker, ETL, Scrap, CLI, Django and becoming an expert in Python.",
+  "p1":"Experienced Co-Founder with a demonstrated history of working in the information technology and services industry. Skilled in Python, SQL, Pandas, and Command Line. Professional with a Bachelor's degree focused in Data Science and Artiﬁcial Intelligence from IESB.",
 }
-
 experienceA = {
   "position" :  "Data Engineer",
   "place" :  "Ministery of Communication",
@@ -53,37 +49,19 @@ experienceA = {
   "started" :  "November 2021",
   "description" :  "Support the analysis of problems and technical solutions of data integration on projects related to internet connectivity in the national.",
 }
-
 experienceB ={
-  "position" :  "President of the junior enterprise.",
+  "position" :  "President of a junior enterprise.",
   "place" :  "DatAí",
-  "finished" :  "April 2022",
+  "finished" :  "now",
   "started" :  "July 2021",
   "description" :  "DatAí is a Junior company of IESB University, that aims to provide a business experience to students of Data Science and related areas.",
 }
-
-# pythonLst =
-  
-# sqlLst = 
-
-# linuxLst = 
-
-# # backendLst = [
-# #   "Django",
-# #   "HTML",
-# #   "CSS"]
-
-# cloudLst = [
-#   "AWS",
-#   ]
-
 skills = {
   "Python":["Pandas","Subprocess","Plotly","Boto3","PySpark",], 
   "SQL":["PostgreSQL","SQLlite",], 
   "Linux":["Bash","Command Line","Data Processing in Shell"], 
   "Cloud":["AWS"],
 }
-
 context = {
     "contact" : personalData,
     "experienceA" : experienceA,
