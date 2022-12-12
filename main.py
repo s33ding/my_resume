058d96b8fc1b02e151e38f7c626fb93f692ea59f
s33ding <robertomdiniz@protonmail.com>
2022-12-12 19:26:16 -0300

    saving

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ certificates['date'] = certificates['date'].apply(lambda x: pd.to_datetime(x))
 certificates = certificates.sort_values('date', ascending=False).reset_index(drop=True)
 certificates.head()
 #%%
-#%%
 certificates = certificates.to_html() #README.md
-with open("teste.md",'w') as f:
+with open("README.md",'w') as f:
     f.write(certificates)
-# %%
