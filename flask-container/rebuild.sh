ac5c076234369f8fc8f7140443b225130ffc05fb
s33ding <robertomdiniz@protonmail.com>
2023-03-11 22:02:33 +0000

    saving

--- flask-container/rebuild.sh
+++ flask-container/rebuild.sh
@@ -1,5 +1,7 @@
 docker rm -f resume
 docker rmi img-resume
+
+python create_home_html.py
 sudo docker build . -t img-resume
 sudo docker run --name resume -d -p 8000:8000 --restart unless-stopped img-resume
 
